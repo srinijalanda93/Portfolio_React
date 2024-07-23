7e982be9cf03a61f8ec3667f636269bd61547f8c
srinija landa <srinijalanda@gmail.com>
2024-07-23 18:58:21 +0530

    left

---
+++ portfolio/src/Components/experience.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Creating a function component which is named
+export const Experience = () => {
+  return (
+    <>
+      <h1>Hello, my name is Srinija Landa</h1>
+    </>
+  );
+};
